c842bd1d060fe0ec6b15ce8db3e18f78f80a80ac
JIYOON <ejiyoon37@email.com>
2025-11-02 00:48:50 +0900

    모집공고, 검색 에러 수정하고 로그인 토큰추가 필요

--- src/components/ui/Field/List_btnsheet.tsx
+++ src/components/ui/Field/List_btnsheet.tsx
@@ -3,9 +3,9 @@ import CheckIcon from '../../../assets/icon/icn_check_24-2.svg?react';
 
 
 interface BottomSheetListItemProps {
-  children: React.ReactNode; // 리스트 아이템에 표시될 텍스트
-  isActive?: boolean;         // 활성화(선택된) 상태 여부
-  onClick: () => void;        // 아이템을 클릭했을 때 실행될 함수
+  children: React.ReactNode; 
+  isActive?: boolean;        
+  onClick: () => void;       
 }
 
 const BottomSheetListItem = ({ 
@@ -18,7 +18,6 @@ const BottomSheetListItem = ({
       onClick={onClick}
       className="flex items-center justify-between w-full h-14 px-2 py-4 bg-white border-b border-gray-100"
     >
-      {/* 텍스트 부분 */}
       <span 
         className={`
           text-base font-medium leading-[1.35] tracking-[-0.03em]
@@ -28,7 +27,6 @@ const BottomSheetListItem = ({
         {children}
       </span>
       
-      {/* 체크 아이콘 부분 (활성화 상태일 때만 보임) */}
       {isActive && (
         <CheckIcon className="w-6 h-6 text-blue-400" />
       )}
