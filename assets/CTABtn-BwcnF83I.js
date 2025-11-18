faa51986b081ded8be5a72b8c119b866502d4901
rryunn <rryunn@users.noreply.github.com>
2025-11-18 16:09:04 +0000

    deploy: b822653e2eeda1752f67ad54e8c021b5f94779f4

---
+++ assets/CTABtn-BwcnF83I.js
@@ -0,0 +1,5 @@
+import{j as l}from"./index-DxpxDltw.js";const i=({isActive:t,children:e,className:s="",...n})=>{const o=`
+    w-full h-12 px-3 py-2.5 rounded-lg
+    font-sans font-semibold text-base leading-[1.35] tracking-[-0.03em]
+    transition-colors
+  `,a=t?"bg-[#3168FF] text-white":"bg-[#A0AAB0] text-white";return l.jsx("button",{className:`${o} ${a} ${s}`,disabled:!t,...n,children:e})};export{i as C};
--- assets/user-UanihtbH.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./index-Ck0rkagM.js";const a=async()=>(await t.get("/api/user/me")).data,r=async()=>(await t.get("/api/recruitments/favorites")).data;export{r as a,a as g};
